be984dec6b8efe6605a0ea57df3fecbeb45a1891
Martin <nitromagix@gmail.com>
2022-02-02 21:56:56 -0800

    added clear button

--- index.js
+++ index.js
@@ -1,41 +1,29 @@
+let white = 'white'
 let color = 'blue'
 
 let colorChoice = document.querySelectorAll('.color-choice')
-
 for (let i = 0; i < colorChoice.length; i++)
 {
     colorChoice[i].addEventListener('click', function(e){
         color = e.target.id
-        console.log(color)
     })
     
 }
 
-/* let red = document.querySelector('#red')
-red.addEventListener('click', function(e){
-    console.log(e.target.id)
-})
-
-let green = document.querySelector('#green')
-green.addEventListener('click', function(e){
-    console.log(e.target.id)
-})
-
-let yellow = document.querySelector('#yellow')
-yellow.addEventListener('click', function(e){
-    console.log(e.target.id)
-})
- */
 var painting = document.querySelector('.painting')
 painting.addEventListener('click', function(e){
     e.target.style.backgroundColor = color
 })
 painting.addEventListener('dblclick', function(e){
-    e.target.style.backgroundColor = 'white'   
+    e.target.style.backgroundColor = white   
 })
 
-/* var pixels = document.querySelector('pixel')
-for (let i = 0; i < pixels.length; i++)
-{
-    pixels[i].addEventListener('cli')
-} */
+let pixels = document.querySelectorAll('.pixel')
+
+var clearBtn = document.querySelector('#clear')
+clearBtn.addEventListener('click', function(){
+    for (let i = 0; i < pixels.length; i++)
+    {
+        pixels[i].style.backgroundColor = white    
+    }
+})
